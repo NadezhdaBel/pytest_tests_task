043174b9e63f72f1892e23b9188c047890e3f37e
Nadezhda Belozorova <balpisv@gmail.com>
2022-09-12 21:25:31 +0300

    Add files via upload

--- test_automation_pytest.py
+++ test_automation_pytest.py
@@ -5,7 +5,7 @@ import pytest
 
 def email_formatting(email=''):
     if re.search(r"[a-zA-Z0-9]+@[a-zA-Z]+\.(ru|com)", email):
-        pass
+        return True
     else:
         return False
 
@@ -16,3 +16,19 @@ def test_strig_email_negative(email_data):
     assert email_formatting(email=email_data) is False
 
 
+def test_strig_email_positive():
+    assert email_formatting(email='some-email@mail.ru') is True
+
+
+def test_list_even():
+    list = [2, 6, 8, 246]
+    for x in list:
+        try:
+            assert x % 2 == 0
+        except:
+            raise ValueError
+
+
+def test_list_names():
+    list = ['Иван', 'Дмитрий', 'Василий']
+    assert 'Дмитрий' in list
